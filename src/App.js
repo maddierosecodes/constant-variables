9334d88942654f5af374cba4806f31a4a1f4c6ab
Emily Bennett <emily.bennett@northcoders.com>
2022-06-11 22:09:33 +0100

    updating fake user data

--- src/App.js
+++ src/App.js
@@ -1,41 +1,42 @@
-import './App.css';
-import { useState } from 'react';
-import { Route, Routes, Link } from 'react-router-dom';
-import { UserContext } from './contexts/User';
-import Login from './components/Login';
-import Header from './components/Header';
-import Homepage from './components/Homepage';
-import ListOfRides from './components/ListOfRides';
-import DriverReview from './components/DriverProfile';
+import "./App.css";
+import { useState } from "react";
+import { Route, Routes, Link } from "react-router-dom";
+import { UserContext } from "./contexts/User";
+import Login from "./components/Login";
+import Header from "./components/Header";
+import Homepage from "./components/Homepage";
+import ListOfRides from "./components/ListOfRides";
+import DriverReview from "./components/DriverProfile";
 import PostRide from "./components/PostRide";
 
 function App() {
   const [user, setUser] = useState({
-    username: 'Ira Martinez',
-    role: 'driver',
-    postcode: 'm1',
+    username: "Ira Martinez",
+    isDriver: true,
+    postcode: "m1",
     isLoggedIn: false,
-    email: "ira.martinez@gmail.com"
+    email: "ira.martinez@gmail.com",
+    uid: "DlvzNfVsaZXHeRV8dujxHWg3ehD3",
   });
   return (
     <UserContext.Provider value={{ user, setUser }}>
-      <main className='App'>
+      <main className="App">
         <Header />
 
         <nav>
           <Link
-            className='f4 fw6 db purple no-underline underline-hover'
-            to='/login'
+            className="f4 fw6 db purple no-underline underline-hover"
+            to="/login"
           >
-            {user.isLoggedIn ? '' : 'Log in here'}
+            {user.isLoggedIn ? "" : "Log in here"}
           </Link>
         </nav>
 
         <Routes>
-          <Route path='/login' element={<Login />} />
-          <Route path='/home' element={<Homepage />} />
-          <Route path='/rides' element={<ListOfRides />} />
-            <Route path="/post" element={<PostRide />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Homepage />} />
+          <Route path="/rides" element={<ListOfRides />} />
+          <Route path="/post" element={<PostRide />} />
         </Routes>
       </main>
     </UserContext.Provider>
