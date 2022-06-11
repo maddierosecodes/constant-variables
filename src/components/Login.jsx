ae4be708dabf8e6068602ed47604e5f6b19cbc47
August Aldred <hachigatsu14@outlook.com>
2022-06-11 13:58:49 +0100

    set up routing

---
+++ src/components/Login.jsx
@@ -0,0 +1,10 @@
+export default function Login() {
+  return (
+    <form action='post'>
+      <label htmlFor='username'>Username</label>
+      <input type='text' id='username' name='username' />
+      <label htmlFor='password'>Password</label>
+      <input type='password' id='password' name='password' />
+    </form>
+  );
+}
