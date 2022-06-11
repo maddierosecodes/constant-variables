b1d25e8b722fc7ec22db914883e05bbc45615eae
Emily Bennett <emily.bennett@northcoders.com>
2022-06-11 16:01:22 +0100

    created post form

--- src/components/PostRide.jsx
+++ src/components/PostRide.jsx
@@ -1,7 +1,40 @@
 import React from "react";
 
 function PostRide() {
-  return <div></div>;
+  return (
+    <div>
+      <h2>Post</h2>
+      <form>
+        <label htmlFor="passengers">Passengers</label>
+        <input
+          type="number"
+          placeholder="How many people?"
+          id="passengers"
+        ></input>
+
+        <label htmlFor="destination">Destination</label>
+        <input
+          type="number"
+          placeholder="Where are you going?"
+          id="destination"
+        ></input>
+
+        <label htmlFor="date">Date</label>
+        <input type="date" id="date"></input>
+
+        <label htmlFor="time">Time</label>
+        <input type="time" id="date"></input>
+
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email"></input>
+
+        <label htmlFor="postcode">Postcode</label>
+        <input type="text" id="postcode"></input>
+
+        <button type="submit">Post</button>
+      </form>
+    </div>
+  );
 }
 
 export default PostRide;
