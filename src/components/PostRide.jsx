c353bc2464d8d50bf5c5d6babac2b209aa7f4f80
Emily Bennett <emily.bennett@northcoders.com>
2022-06-11 17:44:57 +0100

    adding validation code

--- src/components/PostRide.jsx
+++ src/components/PostRide.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
+import { isValid, isValid as isValidPostcode } from "postcode";
 
 function PostRide() {
   const initialForm = {
@@ -8,8 +9,16 @@ function PostRide() {
     email: "",
     postcode: "",
   };
+  const initialValidStates = {
+    passengers: "pending",
+    destination: "pending",
+    dateTime: "pending",
+    email: "pending",
+    postcode: "pending",
+  };
 
   const [advert, setAdvert] = useState(initialForm);
+  const [validStates, setValidStates] = useState(initialValidStates);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -20,20 +29,69 @@ function PostRide() {
     const changeAdvertState = (e) => {
       setAdvert((currentAdvert) => {
         const copy = { ...currentAdvert };
-        copy[key] = e.target.value;
+        const newValue =
+          key === "passengers" ? Number(e.target.value) : e.target.value;
+        copy[key] = newValue;
         return copy;
       });
     };
     return changeAdvertState;
   };
+
+  const validateInput = (key) => {
+    const validateInputByKey = () => {
+      console.log(key);
+      setValidStates((currentValidStates) => {
+        const copyStates = { ...currentValidStates };
+
+        if (key === "passengers") {
+          if (advert.passengers > 0 && advert.passengers < 7) {
+            copyStates.passengers = "true";
+          } else {
+            copyStates.passengers = "false";
+          }
+        }
+
+        if (key === "dateTime") {
+          if (new Date(advert.dateTime) > Date.now()) {
+            copyStates.dateTime = "true";
+          } else {
+            copyStates.dateTime = "false";
+          }
+        }
+
+        if (key === "destination" || key === "postcode") {
+          if (isValidPostcode(advert.destination)) {
+            copyStates[key] = "true";
+          } else {
+            copyStates[key] = "false";
+          }
+        }
+
+        if (key === "email") {
+          const emailRegex =
+            /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+          if (emailRegex.test(advert.email)) {
+            copyStates.email = "true";
+          } else {
+            copyStates.email = "false";
+          }
+        }
+        return copyStates;
+      });
+    };
+    return validateInputByKey;
+  };
+
   return (
     <div>
       <h2>Post</h2>
       <form>
         <label htmlFor="passengers">Passengers</label>
         <input
-          value={advert.passengers}
+          value={advert.passengers.toString()}
           onChange={handleChange("passengers")}
+          onBlur={validateInput("passengers")}
           type="number"
           placeholder="How many people?"
           id="passengers"
@@ -44,6 +102,7 @@ function PostRide() {
         <input
           value={advert.destination}
           onChange={handleChange("destination")}
+          onBlur={validateInput("destination")}
           type="text"
           placeholder="What is the full postcode?"
           id="destination"
@@ -56,6 +115,7 @@ function PostRide() {
           id="date"
           value={advert.dateTime}
           onChange={handleChange("dateTime")}
+          onBlur={validateInput("dateTime")}
           required={true}
         ></input>
 
@@ -66,6 +126,7 @@ function PostRide() {
           value={advert.email}
           onChange={handleChange("email")}
           required={true}
+          onBlur={validateInput("email")}
         ></input>
 
         <label htmlFor="postcode">Postcode</label>
@@ -74,6 +135,7 @@ function PostRide() {
           id="postcode"
           value={advert.postcode}
           onChange={handleChange("postcode")}
+          onBlur={validateInput("postcode")}
           required={true}
         ></input>
 
