c51bced302f4418e599d3f1f0401b3e2b6895f2b
DCrawley94 <duncan.crawley@gmail.com>
2022-06-11 18:20:06 +0100

    update data generation

--- src/dummy-data/local-data/helpers/createDrivers.js
+++ src/dummy-data/local-data/helpers/createDrivers.js
@@ -1,6 +1,7 @@
 const { faker } = require("@faker-js/faker");
 const postcodes = require("./postcodes");
 const ageGroups = ["18-29", "30-50", "50+"];
+const genders = ["male", "female", "non-binary"];
 
 function createDrivers(amount) {
 	const drivers = [];
@@ -8,17 +9,17 @@ function createDrivers(amount) {
 	for (let i = 0; i <= amount; i++) {
 		const driver = {
 			username: faker.internet.userName(),
-			ageGroup: ageGroups[Math.floor(Math.random() * (0 - 3 + 1) + 3)],
+			ageGroup: ageGroups[Math.floor(Math.random() * (0 - 2 + 1) + 2)],
 			backgroundChecks: {
 				dbs: true,
 				licensed: true
 			},
 			carReg: faker.vehicle.vrm(),
 			firstName: faker.name.firstName(),
-			gender: "male",
+			gender: genders[Math.floor(Math.random() * (0 - 2 + 1) + 2)],
 			isDriver: true,
 			lastName: faker.name.lastName(),
-			postcode: postcodes[Math.floor(Math.random() * (0 - 19 + 1) + 19)],
+			postcode: postcodes[Math.floor(Math.random() * (0 - 18 + 1) + 18)],
 			profile: faker.lorem.paragraphs()
 		};
 		drivers.push(driver);
--- src/dummy-data/local-data/helpers/createPassengers.js
+++ src/dummy-data/local-data/helpers/createPassengers.js
@@ -1,6 +1,7 @@
 const { faker } = require("@faker-js/faker");
 const postcodes = require("./postcodes");
 const ageGroups = ["18-29", "30-50", "50+"];
+const genders = ["male", "female", "non-binary"];
 
 function createPassengers(amount) {
 	const passengers = [];
@@ -10,7 +11,7 @@ function createPassengers(amount) {
 			username: faker.internet.userName(),
 			ageGroup: ageGroups[Math.floor(Math.random() * (0 - 3 + 1) + 3)],
 			firstName: faker.name.firstName(),
-			gender: "male",
+			gender: genders[Math.floor(Math.random() * (0 - 2 + 1) + 2)],
 			isDriver: false,
 			lastName: faker.name.lastName(),
 			postcode: postcodes[Math.floor(Math.random() * (0 - 19 + 1) + 19)],
