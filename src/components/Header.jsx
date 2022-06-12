75796cfecd0a2d32811fc8940e37ccb9360d2745
August Aldred <hachigatsu14@outlook.com>
2022-06-12 12:09:22 +0100

    fix home link based on log in state

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,22 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import topLogo from "../assets/images/SpareSeatsHorizontal.png";
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import topLogo from '../assets/images/SpareSeatsHorizontal.png';
+import { UserContext } from '../contexts/User';
 
 export default function Header() {
+  const { user } = useContext(UserContext);
+
   return (
-    <header id="header">
-      <img src={topLogo} alt="spare seats logo" className="headerLogo"></img>
-      <nav className="navBar">
-        <Link to="/home" className="navLink">
+    <header id='header'>
+      <img src={topLogo} alt='spare seats logo' className='headerLogo'></img>
+      <nav className='navBar'>
+        <Link to={user.isLogged ? '/home' : '/'} className='navLink'>
           <h2>Home</h2>
         </Link>
-        <Link to="/rides" className="navLink">
+        <Link to='/rides' className='navLink'>
           <h2>Rides</h2>
         </Link>
-        <Link to="/post" className="navLink">
+        <Link to='/post' className='navLink'>
           <h2>Post</h2>
         </Link>
       </nav>
