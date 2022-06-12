b00fc10de9df9031b301919276b09b1deae34a86
Carrie Platten <carriep@Carries-MacBook-Pro.local>
2022-06-12 03:25:17 +0100

    fetch other user data from db and combine profile page components

--- src/App.js
+++ src/App.js
@@ -1,49 +1,44 @@
-import './App.css';
-import { useState } from 'react';
-import { Route, Routes, Link } from 'react-router-dom';
-import { UserContext } from './contexts/User';
-import Login from './components/Login';
-import Header from './components/Header';
-import Homepage from './components/Homepage';
-import ListOfRides from './components/ListOfRides';
-import SignUp from './components/SignUp';
-import AdditionalSignUp from './components/AdditionalSignUp';
-import Splash from './components/Splash';
-import PostRide from './components/PostRide';
-import DriverProfilePage from './components/DriverProfilePage';
-import PassengerProfilePage from './components/PassengerProfilePage';
+import "./App.css";
+import { useState } from "react";
+import { Route, Routes, Link } from "react-router-dom";
+import { UserContext } from "./contexts/User";
+import Login from "./components/Login";
+import Header from "./components/Header";
+import Homepage from "./components/Homepage";
+import ListOfRides from "./components/ListOfRides";
+import SignUp from "./components/SignUp";
+import AdditionalSignUp from "./components/AdditionalSignUp";
+import Splash from "./components/Splash";
+import PostRide from "./components/PostRide";
+import OtherUserProfilePage from "./components/OtherUserProfilePage";
 
 function App() {
   const [user, setUser] = useState({
-    username: 'Ira Martinez',
-    role: 'driver',
-    postcode: 'm1',
+    username: "Ira Martinez",
+    role: "driver",
+    postcode: "m1",
     isLoggedIn: false,
-    email: 'ira.martinez@gmail.com',
+    email: "ira.martinez@gmail.com",
   });
   return (
     <UserContext.Provider value={{ user, setUser }}>
-      <main className='App'>
+      <main className="App">
         <Header />
 
         <Routes>
-          <Route path='/' element={<Splash />}></Route>
-          <Route path='/login' element={<Login />} />
-          <Route path='/sign-up' element={<SignUp />} />
-          <Route path='/home' element={<Homepage />} />
-          <Route path='/rides' element={<ListOfRides />} />
+          <Route path="/" element={<Splash />}></Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/home" element={<Homepage />} />
+          <Route path="/rides" element={<ListOfRides />} />
           <Route
-            path='/additional-questions'
+            path="/additional-questions"
             element={<AdditionalSignUp />}
           ></Route>
-          <Route path='/post' element={<PostRide />}></Route>
+          <Route path="/post" element={<PostRide />}></Route>
           <Route
-            path='/profile/driver/:userID'
-            element={<DriverProfilePage />}
-          />
-          <Route
-            path='/profile/passenger/:userID'
-            element={<PassengerProfilePage />}
+            path="/profile/:role/:userID"
+            element={<OtherUserProfilePage />}
           />
         </Routes>
       </main>
